714a2ee8ceb12a2f9d061b51b60f07900e7df497
Артём Минидогов <agario.po@mail.ru>
2022-03-15 20:00:24 +0200

    Ready

--- Snakee/Snakee/Program.cs
+++ Snakee/Snakee/Program.cs
@@ -1,58 +1,77 @@
-﻿using System;
+﻿using Snakee;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Snakee
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.SetBufferSize(180, 80);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetBufferSize(180, 80);
 
-            Horizon upLine = new Horizon(0,78,0,'+');
-            Horizon downLine = new Horizon(0, 78, 24, '+');
-            Vertical leftLine = new Vertical(0,24,0,'+');
-            Vertical rightLine = new Vertical(0,24,78,'+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
 
-            Point p = new Point(4, 5, '*');
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
 
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
 
-            while (true)
-            {
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
-                    snake.Move();
-                }
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
 
-                Thread.Sleep(100);
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                }
-                Thread.Sleep(100);
-                snake.Move();
 
-            }
-        }
-    }
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("---------------------------", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Автор: Артём Волков", xOffset + 2, yOffset++);
+			WriteText("Специально для GeekBrains", xOffset + 1, yOffset++);
+			WriteText("---------------------------", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+	}
 }
