783203dc438c6e070a39cc976f3d60179e0a8228
ArtjomVolkov <93324363+ArtjomVolkov@users.noreply.github.com>
2022-03-22 14:16:51 +0200

    Update Media.cs

--- Snakee/Snakee/Media.cs
+++ Snakee/Snakee/Media.cs
@@ -13,11 +13,11 @@ namespace Snakee
         public Media()
         {
             var ind = Directory.GetCurrentDirectory().ToString()
-                .IndexOf("bin", StringComparison.Ordinal);
+                .IndexOf("bin", StringComparison.Ordinal); // Получить индекс папки bin
             string binFolder =
                 Directory.GetCurrentDirectory().ToString().Substring(0, ind)
                 .ToString();
-            resourcesFoler = binFolder + "resources\\";
+            resourcesFoler = binFolder + "resources\\"; // путь до указанной в инкесе папки
         }
         public string GetResourceFolder()
         {
