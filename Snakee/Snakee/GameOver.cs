e75f28435f18a606ad347c2d01ec05bd96f3756c
ArtjomVolkov <93324363+ArtjomVolkov@users.noreply.github.com>
2022-03-22 14:16:04 +0200

    Update GameOver.cs

--- Snakee/Snakee/GameOver.cs
+++ Snakee/Snakee/GameOver.cs
@@ -10,18 +10,18 @@ namespace Snakee
 	{
 
 
-		public void WriteGameOver()
+		public void WriteGameOver() //метод,который пишет в конце игре по типу GameOver
 		{
-			int xOffset = 25;
-			int yOffset = 8;
-			Console.ForegroundColor = ConsoleColor.Green;
+			int xOffset = 25; //кординаты по x
+			int yOffset = 8; //кординаты по y
+			Console.ForegroundColor = ConsoleColor.Green; //цвет текста
 			Console.SetCursorPosition(xOffset, yOffset++);
-			WriteText("============================", xOffset, yOffset++);
-			WriteText("    G A M E    O V E R", xOffset + 1, yOffset++);
-			WriteText("============================", xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++); //текст
+			WriteText("    G A M E    O V E R", xOffset + 1, yOffset++); //текст
+			WriteText("============================", xOffset, yOffset++); //текст
 		}
 
-		static void WriteText(String text, int xOffset, int yOffset)
+		static void WriteText(String text, int xOffset, int yOffset) //метод, написание текста
 		{
 			Console.SetCursorPosition(xOffset, yOffset);
 			Console.WriteLine(text);
